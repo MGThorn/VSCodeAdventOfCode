8893656b17044dad911287652a363ffee10b3602
MGThorn <107687299+MGThorn@users.noreply.github.com>
2023-12-07 16:37:46 +0100

    unbfinsuhed

--- AdventOfCode_2023/src/day3/Day3.java
+++ AdventOfCode_2023/src/day3/Day3.java
@@ -3,25 +3,57 @@ package day3;
 import day.Day;
 
 public class Day3 extends Day {
-    String[][] field;
+    char[] [] field;
+    int [] directions;
+
+    Day3(){
+        directions = new int[8];
+    }
 
     public void initField() {
+        int i =0;
+        field = new char[this.scan("/day3/test_input.txt").size()][this.scan("/day3/test_input.txt").get(0).length()];
+        
         for (String line : this.scan("/day3/test_input.txt")) {
-            String[] lineArr = line.split("");
-            for (String string : lineArr) {
-                System.out.println(string);
-            }
+            char [] temp = line.toCharArray();
+            field[i++] = temp;
         }
 
     }
 
-    public int[] findNumber() {
-        return null;
-
+    public void run(){
+        int sum = 0;
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[0].length; j++) {
+                if (Character.isDigit(field[i][j])&&isValid(i,j)) {
+                    sum += getNumber();
+                }
+            }
+        }
+        System.out.println(sum);
     }
 
-    public int[][] checkSurroundings() {
-        return null;
+    private int getNumber() {
+        return 0;
+    }
+
+    public boolean isValid(int i, int j) {
+        if (!Character.isDigit(field[i-1][j+1])) {
+            return true;
+        }else if (!Character.isDigit(field[i][j+1])) {
+            
+        }else if (!Character.isDigit(field[i+1][j+1])) {
+            
+        }else if (!Character.isDigit(field[i][j-1])) {
+            
+        }else if (!Character.isDigit(field[i][j-1])) {
+            
+        }else if (!Character.isDigit(field[i][j])) {
+            
+        }else if (!Character.isDigit(field[i][j])) {
+            
+        }
+        return true;
 
     }
 }
--- AdventOfCode_2023/src/day3/Main.java
+++ AdventOfCode_2023/src/day3/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Day3 day3 = new Day3();
         day3.initField();
+        day3.run();
     }
 }
