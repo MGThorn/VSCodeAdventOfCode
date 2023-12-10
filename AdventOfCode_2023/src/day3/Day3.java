ff6ef8e5d9b5afd875f63d9ba10814b02b8d9d79
MGThornHomePC <Lennart.Dorn1@outlook.de>
2023-12-10 14:56:01 +0100

    working for example but not for real input

--- AdventOfCode_2023/src/day3/Day3.java
+++ AdventOfCode_2023/src/day3/Day3.java
@@ -5,16 +5,18 @@ import day.Day;
 public class Day3 extends Day {
     char[] [] field;
     int [] directions;
+    int stackNumber;
 
     Day3(){
         directions = new int[8];
+        stackNumber = 0;
     }
 
     public void initField() {
         int i =0;
-        field = new char[this.scan("/day3/test_input.txt").size()][this.scan("/day3/test_input.txt").get(0).length()];
+        field = new char[this.scan("/day3/input.txt").size()][this.scan("/day3/input.txt").get(0).length()];
         
-        for (String line : this.scan("/day3/test_input.txt")) {
+        for (String line : this.scan("/day3/input.txt")) {
             char [] temp = line.toCharArray();
             field[i++] = temp;
         }
@@ -23,37 +25,49 @@ public class Day3 extends Day {
 
     public void run(){
         int sum = 0;
+        stackNumber = 0;
+        boolean validNumber = false;
+       
+
         for (int i = 0; i < field.length; i++) {
-            for (int j = 0; j < field[0].length; j++) {
-                if (Character.isDigit(field[i][j])&&isValid(i,j)) {
-                    sum += getNumber();
+            for (int j = 0; j < field[i].length; j++) {
+                if (Character.isDigit(field[i][j])) {
+                    stackNumber = stackNumber*10 + Character.getNumericValue(field[i][j]);
+                    validNumber = validNumber ? true : isValid(i, j); 
+                }else{
+                    if (validNumber) {
+                        sum += stackNumber;
+                    }
+                    validNumber = false;
+                    stackNumber = 0;
                 }
             }
         }
         System.out.println(sum);
     }
 
-    private int getNumber() {
-        return 0;
-    }
+    
 
     public boolean isValid(int i, int j) {
-        if (!Character.isDigit(field[i-1][j+1])) {
-            return true;
-        }else if (!Character.isDigit(field[i][j+1])) {
-            
-        }else if (!Character.isDigit(field[i+1][j+1])) {
-            
-        }else if (!Character.isDigit(field[i][j-1])) {
-            
-        }else if (!Character.isDigit(field[i][j-1])) {
-            
-        }else if (!Character.isDigit(field[i][j])) {
-            
-        }else if (!Character.isDigit(field[i][j])) {
-            
+        boolean ret=false;
+        if (i-1>=0&&j+1<field.length&&!(field[i-1][j+1]=='.'|Character.isDigit(field[i-1][j+1]))) {
+            ret = true;
+        }else if (j+1<field.length&&!(field[i][j+1]=='.'|Character.isDigit(field[i][j+1]))) {
+            ret = true;
+        }else if (j+1<field.length&&i+1<field.length&&!(field[i+1][j+1]=='.'|Character.isDigit(field[i+1][j+1]))) {
+            ret = true;
+        }else if (i+1<field.length&&!(field[i+1][j]=='.'|Character.isDigit(field[i+1][j]))) {
+            ret = true;
+        }else if (i-1>=0&&!(field[i-1][j]=='.'|Character.isDigit(field[i-1][j]))) {
+            ret = true;
+        }else if (j-1>=0&&i-1>=0&&!(field[i-1][j-1]=='.'|Character.isDigit(field[i-1][j-1]))) {
+            ret = true;
+        }else if (j-1>=0&&!(field[i][j-1]=='.'|Character.isDigit(field[i][j-1]))) {
+            ret = true;
+        }else if (j-1>=0&&i+1<field.length&&!(field[i+1][j-1]=='.'|Character.isDigit(field[i+1][j-1]))) {
+            ret = true;
         }
-        return true;
+        return ret ;
 
     }
 }
--- AdventOfCode_2023/src/day3/Main.java
+++ AdventOfCode_2023/src/day3/Main.java
@@ -3,7 +3,7 @@ package day3;
 public class Main {
     public static void main(String[] args) {
         Day3 day3 = new Day3();
-        day3.initField();
-        day3.run();
+        day3.initField(); 
+        day3.run(); // works for example but not for real input
     }
 }
