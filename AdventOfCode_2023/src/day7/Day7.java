53bc5c678a8d37b8d202d21e05baf08625d6e946
MGThorn <107687299+MGThorn@users.noreply.github.com>
2023-12-11 19:12:16 +0100

    f

--- AdventOfCode_2023/src/day7/Day7.java
+++ AdventOfCode_2023/src/day7/Day7.java
@@ -9,7 +9,7 @@ public class Day7 extends Day {
     Map<String, Integer> betMap = new HashMap<>();
 
     public void init(){
-        for (String line : this.scan("/day7/test_input.txt")) {
+        for (String line : this.scan("/day7/input.txt")) {
             betMap.put(line.split(" ")[0], Integer.parseInt(line.split(" ")[1]));
         }
         
