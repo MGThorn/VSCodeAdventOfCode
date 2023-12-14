e88ff102b47199b7b897d773f641a0250e7451bd
MGThornHomePC <Lennart.Dorn1@outlook.de>
2023-12-14 22:42:14 +0100

    dwqd

--- AdventOfCode_2023/src/day8/Day8.java
+++ AdventOfCode_2023/src/day8/Day8.java
@@ -4,42 +4,61 @@ import day.Day;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.ArrayList;
-import java.util.Arrays;
+
 
 public class Day8 extends Day{
     
+    
     Map<String, Map<Character, String>> directions = new HashMap<>();
-    String leftRigthString; 
+    String leftRigthString;
+    String START; 
+    String LAST;
 
     
     public void init() {
-        for (Map<Character, String> direction : directions.values()) {
-            direction = new HashMap<>();
-        }
-
         leftRigthString = "";
         ArrayList<String> scan = scan("/day8/input.txt");
         leftRigthString += scan.get(0);
         leftRigthString += scan.get(1);
         scan.remove(0);
-        scan.remove(1);
+        
 
         String[] parts;
 
         for (String line : scan) {
             parts = line.split("[=(),\\s]+");
-            System.out.print(parts.length != 3 ? line : "");
+            if (parts.length<3) {
+                continue;
+            }
+            if (START==null) {
+                START = parts[0];
+            }
             String firstKey = parts[0];
-            String firstValue = parts[0];
-            String secondValue = parts[0];
+            String firstValue = parts[1];
+            String secondValue = parts[2];
 
             directions.put(firstKey, new HashMap<>());
             directions.get(firstKey).put('L', firstValue);
             directions.get(firstKey).put('R', secondValue);
-
-            //System.out.println(directions.get(firstKey).get('L'));
-            //System.out.println(directions.get(firstKey).get('R'));
+            LAST = firstKey;
+            System.out.println(secondValue);
         }
         
+        
+    }
+    public int countSteps(){
+        String nexString = START;
+        int counter=0;
+        while (!nexString.equals(LAST)) {
+            for (char c : leftRigthString.toCharArray()) {
+            nexString = directions.get(nexString).get(c);
+            counter++;
+            if (nexString.equals(LAST)) {
+                break;
+            }
+            }
+            
+        }
+        return counter;
     }
 }
--- AdventOfCode_2023/src/day8/Main.java
+++ AdventOfCode_2023/src/day8/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Day8 day8 = new Day8();
         day8.init();
+        System.out.println(day8.countSteps());
     }
 }
