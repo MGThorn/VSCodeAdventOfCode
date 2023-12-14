c99fa1374dfbadec28631954b78f207c8c3ac27a
MGThorn <107687299+MGThorn@users.noreply.github.com>
2023-12-14 15:28:14 +0100

    found mistake first String is empty

--- AdventOfCode_2023/src/day8/Day8.java
+++ AdventOfCode_2023/src/day8/Day8.java
@@ -1,17 +1,17 @@
 package day8;
 
-import day7.Day7;
+import day.Day;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class Day8 extends Day7{
+public class Day8 extends Day{
     
     Map<String, Map<Character, String>> directions = new HashMap<>();
     String leftRigthString; 
 
-    @Override
+    
     public void init() {
         for (Map<Character, String> direction : directions.values()) {
             direction = new HashMap<>();
@@ -23,14 +23,12 @@ public class Day8 extends Day7{
         leftRigthString += scan.get(1);
         scan.remove(0);
         scan.remove(1);
-        scan.remove(2);
+
+        String[] parts;
 
         for (String line : scan) {
-            String[] parts = line.split("[=(),\\s]+");
-            System.out.println(parts.length);
-            for (String s : parts) {
-                System.out.println(s);
-            }
+            parts = line.split("[=(),\\s]+");
+            System.out.print(parts.length != 3 ? line : "");
             String firstKey = parts[0];
             String firstValue = parts[0];
             String secondValue = parts[0];
